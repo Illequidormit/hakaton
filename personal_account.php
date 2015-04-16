e927546fb61886fca6f1190e13d7302352858d79
Illequidormit <pasha9657@gmail.com>
2015-04-16 14:32:46 +0300

    Add item

--- personal_account.php
+++ personal_account.php
@@ -23,8 +23,32 @@ require_once "navbar.php";
 
                 <div class="tab-content">
                     <div id="add-item-tab" class="tab-pane fade in active">
-                        <h3>HOME</h3>
-                        <p>Lorem ipsum dolor sit amet, e et dolore magna aliqua.</p>
+                    <form method="post" enctype="multipart/form-data">
+                        <h5>Title</h5>
+                        <input type="text" name="title" placeholder="title">
+                        <h5>Description</h5>
+                        <textarea placeholder="Description" name="descr"></textarea>
+                        <h5>Category</h5>
+                        <select name="category">
+                            <option value="1">1111</option>
+                            <option value="2">2222</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                        </select>
+                        <input type="file" name="fileToUpload" id="fileToUpload"/>
+                        <input type="submit" name="add_item">
+                    </form>
+                    <?php
+                        include("classes/ItemData.php");
+                        $Item = new ItemData();
+                        if(isset($_POST['add_item'])){
+                            $Item->addItem($_POST['title'], $_POST['descr'], $_POST['category'],$_FILES['fileToUpload']['name']);
+                        }
+                    ?>
                     </div>
                     <div id="offers-tab" class="tab-pane fade">
                         <h3>Menu 1</h3>
