a67a02353b62ad46c9266e4e5a6dabbce8b6473b
Tenfrow <vit2zremb@gmail.com>
2015-04-16 12:59:12 +0300

    Add pictures and templates
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Your branch is ahead of 'origin/master' by 4 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	new file:   images/STRONG-HANDSHAKE.jpg
    #	new file:   templates/header.html
    #
    # Untracked files:
    #	css/style.css
    #	templates/navbar.html
    #

--- index.php
+++ index.php
@@ -26,15 +26,77 @@
         <div class="row">
             <div class="col-sm-8">
                 <div class="row">
-                    <div class="col-sm-3"></div>
-                    <div class="col-sm-3">fff</div>
-                    <div class="col-sm-3">fff</div>
-                    <div class="col-sm-3">fff</div>
+                    <div class="col-sm-3">
+                        <a href="#">
+                            <div class="well category">
+                                <img src="images/category/1.png">
+                                <h4>Kid world</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a href="#">
+                            <div class="well category">
+                                <img src="images/category/2.png">
+                                <h4>Realty</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a href="#">
+                            <div class="well category">
+                                <img src="images/category/3.png">
+                                <h4>Cars</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a href="#">
+                            <div class="well category">
+                                <img src="images/category/4.png">
+                                <h4>Work</h4>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <a href="#">
+                            <div class="well category">
+                                <img src="images/category/5.png">
+                                <h4>Pets</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a href="#">
+                            <div class="well category">
+                                <img src="images/category/6.png">
+                                <h4>Furniture</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a href="#">
+                            <div class="well category">
+                                <img src="images/category/7.png">
+                                <h4>Appliances</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a href="#">
+                            <div class="well category">
+                                <img src="images/category/8.png">
+                                <h4>Services</h4>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
             <div class="col-sm-4">
                 <h4>Швидко, легко, безкоштовно</h4>
-                <button class="btn btn-block btn-primary" value="">Подати заяву</button>
+                <button class="btn btn-block btn-primary">Подати заяву</button>
             </div>
         </div>
     </div>
