a18effedfeaccf08a031e2fd7e61472b47dbaf89
Andriy5852 <andriy5852@gmail.com>
2015-04-16 10:50:53 +0300

    Додати клас бази даних

---
+++ classes/Database.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Using
+ * $db = Database::getInstance();
+ * $command = "command text";
+ * $result = $db->query($command);
+ */
+class Database {
+    protected static $_instance;
+    private $_host = "localhost";
+    private $_username = "root";
+    private $_password = "";
+    private $_database = "bd_swap";
+    private $_connection;
+
+    static public function getInstance()
+    {
+        if(self::$_instance === null) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+    private function __construct()
+    {
+        $this->_connection = new mysqli($this->_host, $this->_username,
+            $this->_password, $this->_database) or die("Database connection error");
+    }
+
+    private function __clone() { }
+
+    public function getConnection()
+    {
+        return $this->_connection;
+    }
+
+    public function query($command)
+    {
+        return $this->getConnection()->query($command);
+    }
+
+    
+}
