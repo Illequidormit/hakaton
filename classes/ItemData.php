3722bd7f7e0d7d50396643c479d42940ac177fa2
Andriy5852 <andriy5852@gmail.com>
2015-04-16 13:51:14 +0300

    Перегляд товарів

--- classes/ItemData.php
+++ classes/ItemData.php
@@ -32,20 +32,34 @@ class ItemData
         $queryResult = $this->db->query($command);
     }
 
-    public function printItem($category)
+    public function printAllItem($category)
     {
-        $command = "SELECT * FROM `item` WHERE `category` = '$category'";
+        $command = "SELECT * FROM `item` WHERE `id_category` = '$category'";
         $queryResult = $this->db->query($command);
         while($row = mysqli_fetch_array($queryResult)) 
-            	echo "<div class="col-sm-3">
-                        <a href='$row['name']'>
+            	echo "<div class='col-sm-3'>
+                        <a href='item.php?item=".$row['id']."'>
                             <div class='well category'>
-                                <img src='images/category/$row['img_url']'>
-                                <h4>Kid world</h4>
+                                <img src='../".$row['img_url']."'>
+                                <h4>".$row['name']."</h4>
                             </div>
                         </a>
-                    </div>"
+                    </div>";
+    }
 
+    public function printItem($id)
+    {
+        $command = "SELECT * FROM `item` WHERE `id` = '$id'";
+        $queryResult = $this->db->query($command);
+        while($row = mysqli_fetch_array($queryResult)) 
+            	echo "<div class='col-sm-5'>
+                                <img src='../".$row['img_url']."' width='300px' heigth='300px'>
+                    </div>
+					<div class='col-sm-7'>
+						<h1>".$row['name']."</h1>
+						<p>".$row['description']."</p>
+					</div>
+                    ";
     }
 
 
