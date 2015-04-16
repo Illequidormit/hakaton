ebf661456b289f6fce02f5355eb46b1c40645d0b
Andriy5852 <andriy5852@gmail.com>
2015-04-16 12:52:55 +0300

    шось покращити

--- user/login.php
+++ user/login.php
@@ -16,7 +16,6 @@
 				include('../classes/Userdata.php');
 				$db = new Userdata();
 				$db->enter($_POST['login'],$_POST['password']);
-				$db->printname();
 				}
 			else{
 				echo "Заповніть поля для реєстрації";
