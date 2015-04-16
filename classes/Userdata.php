ac7c65b1e041d240b9e29de74171ee38d9814d6c
Andriy5852 <andriy5852@gmail.com>
2015-04-16 11:27:49 +0300

    Змынити назву таблички Users - user

--- classes/Userdata.php
+++ classes/Userdata.php
@@ -16,14 +16,14 @@ class Userdata
 
 	public function createUser($login, $password, $mail)
     {
-		$command = "INSERT INTO Users(login, password, mail) VALUES ('".$login."','".md5($password)."','".$mail."')";
+		$command = "INSERT INTO user(login, password, mail) VALUES ('".$login."','".md5($password)."','".$mail."')";
 		$this->db->query($command);
 		echo "Реєстрація успішно проведена";
 	}
 
 	public function enter($login, $password)
     {
-		$queryResult =$this->db->query("SELECT * FROM `Users` WHERE `login` = '$login'");
+		$queryResult =$this->db->query("SELECT * FROM `user` WHERE `login` = '$login'");
         while($row = mysqli_fetch_array($queryResult)) 
             if($row['Password'] == md5($password))        
                 return "user.php";
